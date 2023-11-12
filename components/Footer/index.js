3ef710f4de075fcfebd799a3b7a3dc879976812e
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-11-12 00:27:18 -0800

    added calendly link to schedule a call button

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,6 +4,9 @@ import Link from "next/link";
 import Button from "../Button";
 
 const Footer = ({}) => {
+  const openCalendly = () => {
+    window.open('https://calendly.com/neil-v-holloway', '_blank');
+  };
   return (
     <>
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
@@ -16,7 +19,7 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
-            <Button type="primary">Schedule a call</Button>
+            <Button onClick={openCalendly} type="primary">Schedule a call</Button>
             <div className="mt-10">
               <Socials />
             </div>
